647bf01865c75a0fbb2c69c5853fef210036a92c
Rutvik <rutvikmaniya13@gmail.com>
2020-04-22 10:26:58 +0530

    Module 13

---
+++ Module13/Module 13 Demo/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlString HelloWorld()
+    {
+        return new SqlString("Hello World!");
+    }
+}
